5989e6866b93f9a5c1dac55f2763f443253db7b1
Arthur Song <arth3rs0ng@gmail.com>
2019-04-22 07:31:29 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -37,7 +37,12 @@ class InteractiveRecord
   def values_for_insert
     #I need "'Bob', 9"
     self.class.column_names.map do |column_name|
-      send("#{column_name}")
-    end
+      if column_name.is_a? String 
+        send("'#{column_name}'") #gets self.column_name adds quote for string for insert
+      else
+        send("#{column_name}") #gets self.column_name
+      end
+    end.compact
+    
   end
 end
