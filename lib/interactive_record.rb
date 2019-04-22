81a892769528226895bb3e8de22f2165c6ffaf85
Arthur Song <arth3rs0ng@gmail.com>
2019-04-22 07:14:08 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -12,7 +12,7 @@ class InteractiveRecord
     
     column_names = []
     array_of_column_info = DB[:conn].execute(sql) #returns an array of hashes, hash = column_info
-    arary_of_column_info.each do |column_info|
+    array_of_column_info.each do |column_info|
       column_names << column_info["name"]
     end
     column_names
